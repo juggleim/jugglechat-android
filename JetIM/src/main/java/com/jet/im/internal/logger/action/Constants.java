972ddd760b8d7a53bc738223852010f1243320e4
ZhangZhibin <zzbljh123@163.com>
2024-06-03 16:53:11 +0800

    1. Use HttpsURLConnection to replace OkHttpClient to upload logs;

--- JetIM/src/main/java/com/jet/im/internal/logger/action/Constants.java
+++ JetIM/src/main/java/com/jet/im/internal/logger/action/Constants.java
@@ -13,5 +13,10 @@ class Constants {
     static final String LOG_FILE_SUFFIX = ".jlog";
     static final String ZIP_FILE_SUFFIX = ".zip";
     static final String LOG_TIMESTAMP_FORMAT = "yyyyMMddHH";
+
     static final int LOG_UPLOAD_TIME_OUT = 15 * 1000;
+    static final String LOG_UPLOAD_PREFIX = "--";
+    static final String LOG_UPLOAD_LINE_END = "\r\n";
+    static final String LOG_UPLOAD_CHARSET = "UTF-8";
+    static final String LOG_UPLOAD_KEEP_ALIVE = "keep-alive";
 }
--- JetIM/src/main/java/com/jet/im/internal/logger/action/UploadDefaultRunnable.java
+++ JetIM/src/main/java/com/jet/im/internal/logger/action/UploadDefaultRunnable.java
@@ -1,19 +1,24 @@
 package com.jet.im.internal.logger.action;
 
+import android.annotation.SuppressLint;
+
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.TimeUnit;
+import java.util.UUID;
 
-import okhttp3.MultipartBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
 
 /**
  * @author Ye_Guli
@@ -55,41 +60,75 @@ class UploadDefaultRunnable extends UploadRunnable {
     }
 
     private void doPostRequest(String mUploadUrl, File file, Map<String, String> mRequestHeaders) {
-        //构造OkHttpClient
-        OkHttpClient client = new OkHttpClient.Builder()
-                .connectTimeout(Constants.LOG_UPLOAD_TIME_OUT, TimeUnit.MILLISECONDS)
-                .readTimeout(Constants.LOG_UPLOAD_TIME_OUT, TimeUnit.MILLISECONDS)
-                .writeTimeout(Constants.LOG_UPLOAD_TIME_OUT, TimeUnit.MILLISECONDS)
-                .build();
-        //创建文件请求体
-        RequestBody fileBody = RequestBody.create(file, null);
-        //创建 multipart 请求体
-        MultipartBody.Builder requestBodyBuilder = new MultipartBody.Builder()
-                .setType(MultipartBody.FORM)
-                .addFormDataPart("log", file.getName(), fileBody);
-        //构建请求体
-        RequestBody requestBody = requestBodyBuilder.build();
-        //构建请求
-        Request.Builder requestBuilder = new Request.Builder()
-                .url(mUploadUrl)
-                .post(requestBody);
-        //添加请求头
-        Set<Map.Entry<String, String>> entrySet = mRequestHeaders.entrySet();
-        for (Map.Entry<String, String> tempEntry : entrySet) {
-            requestBuilder.addHeader(tempEntry.getKey(), tempEntry.getValue());
-        }
-        Request request = requestBuilder.build();
-        Response response = null;
+        String boundary = UUID.randomUUID().toString();
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        HttpURLConnection connection = null;
+        ByteArrayOutputStream responseStream;
+        byte[] buffer = new byte[2048];
         try {
-            //同步请求
-            response = client.newCall(request).execute();
-            if (!response.isSuccessful()) {
-                notifyUploadActionCallbackFail(-1, "doPostRequest failed, statusCode= " + response.code());
-                return;
+            URL url = new URL(mUploadUrl);
+            connection = (HttpURLConnection) url.openConnection();
+            if (connection instanceof HttpsURLConnection) {
+                ((HttpsURLConnection) connection).setHostnameVerifier(new HostnameVerifier() {
+                    @SuppressLint("BadHostnameVerifier")
+                    @Override
+                    public boolean verify(String hostname, SSLSession session) {
+                        return true;
+                    }
+                });
             }
-            try {
-                //解析JSON数据
-                String resultData = response.body().string();
+            connection.setReadTimeout(Constants.LOG_UPLOAD_TIME_OUT);
+            connection.setConnectTimeout(Constants.LOG_UPLOAD_TIME_OUT);
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setUseCaches(false);
+            connection.setAllowUserInteraction(false);
+            connection.setChunkedStreamingMode(0);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Charset", Constants.LOG_UPLOAD_CHARSET);
+            connection.setRequestProperty("connection", Constants.LOG_UPLOAD_KEEP_ALIVE);
+            connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+            //添加请求头
+            Set<Map.Entry<String, String>> entrySet = mRequestHeaders.entrySet();
+            for (Map.Entry<String, String> tempEntry : entrySet) {
+                connection.addRequestProperty(tempEntry.getKey(), tempEntry.getValue());
+            }
+            outputStream = connection.getOutputStream();
+            //写入文件参数
+            if (file != null) {
+                String fileParam = new StringBuilder()
+                        .append(Constants.LOG_UPLOAD_PREFIX)
+                        .append(boundary)
+                        .append(Constants.LOG_UPLOAD_LINE_END)
+                        .append("Content-Disposition: form-data; name=\"log\"; filename=\"").append(file.getName()).append("\"").append(Constants.LOG_UPLOAD_LINE_END)
+                        .append("Content-Type: application/octet-stream; charset=").append(Constants.LOG_UPLOAD_CHARSET).append(Constants.LOG_UPLOAD_LINE_END)
+                        .append(Constants.LOG_UPLOAD_LINE_END)
+                        .toString();
+                outputStream.write(fileParam.getBytes());
+                //写入文件
+                FileInputStream fileInputStream = new FileInputStream(file);
+                int bytesRead;
+                while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+                fileInputStream.close();
+                outputStream.write(Constants.LOG_UPLOAD_LINE_END.getBytes());
+            }
+            //请求结束标志
+            byte[] end_data = (Constants.LOG_UPLOAD_PREFIX + boundary + Constants.LOG_UPLOAD_PREFIX + Constants.LOG_UPLOAD_LINE_END).getBytes();
+            outputStream.write(end_data);
+            outputStream.flush();
+            //得到响应码
+            int statusCode = connection.getResponseCode();
+            if (statusCode / 100 == 2) {
+                responseStream = new ByteArrayOutputStream();
+                inputStream = connection.getInputStream();
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    responseStream.write(buffer, 0, bytesRead);
+                }
+                String resultData = responseStream.toString();
                 JSONObject jsonResponse = new JSONObject(resultData);
                 int code = jsonResponse.getInt("code");
                 if (code == 0) {
@@ -97,16 +136,29 @@ class UploadDefaultRunnable extends UploadRunnable {
                 } else {
                     notifyUploadActionCallbackFail(-1, "doPostRequest failed, resultData= " + resultData);
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
-                notifyUploadActionCallbackFail(-1, "doPostRequest failed, e= " + e.getMessage());
+            } else {
+                notifyUploadActionCallbackFail(-1, "doPostRequest failed, statusCode= " + statusCode);
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
             notifyUploadActionCallbackFail(-1, "doPostRequest failed, e= " + e.getMessage());
         } finally {
-            if (response != null) {
-                response.close();
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                connection.disconnect();
             }
         }
     }
