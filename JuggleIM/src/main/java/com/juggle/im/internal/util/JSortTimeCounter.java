871aeccfc0943058c0644210a37c51d574a72241
Fei Li <14392240@qq.com>
2024-08-05 18:50:21 +0800

    remove useless code

--- JuggleIM/src/main/java/com/juggle/im/internal/util/JSortTimeCounter.java
+++ JuggleIM/src/main/java/com/juggle/im/internal/util/JSortTimeCounter.java
@@ -15,23 +15,17 @@ public class JSortTimeCounter {
     private final SharedPreferences mSharedPreferences;
 
     private volatile long mCurrentSortTime;
-    private volatile boolean mIsFirstGet;
 
     public JSortTimeCounter(Context context, String appKey, String userId) {
         mSortTimeKey = String.format("key_%s_%s", appKey, userId);
         mSharedPreferences = context.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE);
         mCurrentSortTime = mSharedPreferences.getLong(mSortTimeKey, 0L);
-        mIsFirstGet = mCurrentSortTime == 0L;
     }
 
     public long getNextSortTime() {
         synchronized (lock) {
-            if (mIsFirstGet) {
-                mIsFirstGet = false;
-                return 0;
-            }
             mCurrentSortTime++;
-            mSharedPreferences.edit().putLong(mSortTimeKey, mCurrentSortTime).commit();
+            mSharedPreferences.edit().putLong(mSortTimeKey, mCurrentSortTime).apply();
             return mCurrentSortTime;
         }
     }
