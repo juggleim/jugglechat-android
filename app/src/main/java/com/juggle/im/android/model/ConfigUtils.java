975bb3ad16d635c09b0072980dc3b8246a72bbf4
Talen <Talen@juggle.im>
2025-11-24 15:35:18 +0800

    build issue

--- app/src/main/java/com/juggle/im/android/model/ConfigUtils.java
+++ app/src/main/java/com/juggle/im/android/model/ConfigUtils.java
@@ -2,7 +2,7 @@ package com.juggle.im.android.model;
 
 public class ConfigUtils {
     public static String appKey = "nwm6fxqt2aeebhb7";
-    public static String appServerUrl = "http://ws.snailchat.im";
+    public static String appServerUrl = "https://ws.snailchat.im";
 
     public static String imServer = "wss://ws.snailchat.im";
 
